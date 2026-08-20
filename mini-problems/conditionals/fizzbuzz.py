ba462f88f87a4c73ad087819bcefcc865f5f013d
Minali Kancharla <minali.kancharla08@gmail.com>
2026-08-20 12:12:45 -0700

    Create fizzbuzz.py

---
+++ mini-problems/conditionals/fizzbuzz.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number: "))
+
+if number % 3 == 0 and number % 5 == 0:
+  print("FizzBuzz")
+elif number % 5 == 0:
+  print("Buzz")
+elif number % 3:
+  print("Fizz")
+else:
+  print(number)
+  
